b1a5d228fd229d3b8ef87165fd5c5ba96781d51b
citizensix <eugene0tall@gmail.com>
2016-10-26 11:58:07 +0300

    4 задача по Python

--- Python/4_sript_arg/4.py
+++ Python/4_sript_arg/4.py
@@ -1,8 +1,18 @@
 import sys
 import os
 import shutil
-dir_name_1 = sys.argv[1]
-dir_name_2 = sys.argv[2]
-extesion = os.path.splitext(filename)[1][1:]
-print(dir_name_1)
-print(dir_name_2)
+dir_1 = sys.argv[1]
+dir_2 = sys.argv[2]
+for root, dirs, files in os.walk(dir_1):
+    for name in files:
+        curr = os.path.join(root, name)
+        if not "." in name:
+            extension = "without_extension"
+        else:
+            extension = name[name.rfind(".")+1:]
+        new_dir = os.path.join(dir_2, extension)
+        if not os.path.exists(new_dir):
+            os.mkdir(new_dir)
+        shutil.move(curr, os.path.join(new_dir, name))
+print(dir_1)
+print(dir_2)
